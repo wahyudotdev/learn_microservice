9000aacdc761788820d548ea674f31734ec24bb5
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-09-03 16:54:35 +0700

    adding docker-compose.yml

---
+++ kubernetes/run-all.sh
@@ -0,0 +1,5 @@
+kubectl apply -f config.yaml
+kubectl apply -f rabbit-mq.yaml
+kubectl apply -f api-gateway.yaml
+kubectl apply -f product-command.yaml
+kubectl apply -f product-query.yaml
---
+++ kubernetes/stop-all.sh
@@ -0,0 +1,5 @@
+kubectl delete -f config.yaml
+kubectl delete -f rabbit-mq.yaml
+kubectl delete -f api-gateway.yaml
+kubectl delete -f product-command.yaml
+kubectl delete -f product-query.yaml
